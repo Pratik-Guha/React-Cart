8da4bad193346c94c6958b71d237984e60487884
Pratik-Guha <sguha4966@gmail.com>
2023-10-17 12:34:07 +0530

    updated

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,11 @@
-import { Box, Button } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 
 const ProductCard = ({name,id,price,handler,imgSrc}) => {
   
   return (
     <div className=' bg-slate-400 w-[200px] m-8 p-8 rounded-md transition-all duration-500 flex flex-col items-center hover:scale-105'>
-        <img src={imgSrc} alt={name} className=' h-full w-full object-cover' />
+        <img src={imgSrc} alt={name} className=' h-32 w-32 rounded-md object-cover' />
         <p>{name}</p>
         <h4 className=' m-4'>${price}</h4>
         <Box as='button' p={'2'} borderRadius={'md'} fontWeight={'bold'} bgGradient='linear(to-r, teal.500, green.500)'  _hover={{
